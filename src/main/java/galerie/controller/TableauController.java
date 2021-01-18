c705dbd83ed182b91935de9d283184c4d96667fc
Stéphane <81stephane@gmail.com>
2021-01-18 13:24:44 +0100

    TP 18/01/2021 1. Affichage HTML Tableaux v1

---
+++ src/main/java/galerie/controller/TableauController.java
@@ -0,0 +1,30 @@
+package galerie.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import galerie.dao.TableauRepository;
+import galerie.entity.Galerie;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping(path = "/tableau")
+public class TableauController {
+    
+    @Autowired
+    private TableauRepository dao;
+    
+    @GetMapping(path = "/show")
+    public String afficheTousLesTableaux(Model model) {
+        model.addAttribute("tableaux", dao.findAll());
+        return "afficheTableaux";
+    }
+    
+}
